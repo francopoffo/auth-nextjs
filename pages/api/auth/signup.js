5784841f1d40c10b358e7be80f863ccb9d5121bd
Franco Poffo <franco_sutter@yahoo.com.br>
2023-03-24 09:10:53 -0300

    Enviando solicitações de signup do formulário

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,27 +2,29 @@ import { connectToDatabase } from "../../../lib/db";
 import { hashPassword } from "../../../lib/auth";
 
 async function handler(req, res) {
-  const data = req.body;
+  if (req.method === "POST") {
+    const data = req.body;
 
-  const { email, password } = data;
+    const { email, password } = data;
 
-  const hashedPassword = hashPassword(password);
+    const hashedPassword = await hashPassword(password);
 
-  if (!email || !email.includes("@") || !password || password.trim() < 7) {
-    res.status(422).json({ message: "Invalid input" });
-    return;
+    if (!email || !email.includes("@") || !password || password.trim() < 7) {
+      res.status(422).json({ message: "Invalid input" });
+      return;
+    }
+
+    const client = connectToDatabase();
+
+    const db = client.db();
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "User created." });
   }
-
-  const client = connectToDatabase();
-
-  const db = client.db();
-
-  const result = await db.collection("users").insertOne({
-    email: email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: "User created." });
 }
 
 export default handler;
